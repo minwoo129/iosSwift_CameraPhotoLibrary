fd6a298f2db4f836cbaeae4cb06dceaf05bddb02
minwoo129 <key0129mw@naver.com>
2023-08-05 16:58:08 +0900

    델리게이트 메서드 추가

--- CameraPhotoLibrary/ViewController.swift
+++ CameraPhotoLibrary/ViewController.swift
@@ -91,6 +91,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString;
+        
+        if(mediaType.isEqual(to: kUTTypeImage as NSString as String)) {
+            captureImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage;
+            
+            if(flagImageSave) {
+                UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
+            }
+            
+            imgView.image = captureImage;
+        }
+        else if(mediaType.isEqual(to: kUTTypeMovie as NSString as String)) {
+            if(flagImageSave) {
+                videoURL = (info[UIImagePickerController.InfoKey.mediaURL] as! URL);
+                UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, self, nil, nil);
+            }
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     // 경고표시용 메서드
     func myAlert(_ title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
